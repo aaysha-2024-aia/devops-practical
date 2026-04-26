aec23029c277d8d123d27be3c6a8795105f85f7a
Aaysha <your-email@example.com>
2026-04-26 20:19:02 +0530

    My assignment files

---
+++ prac.py
@@ -0,0 +1,20 @@
+def remove_duplicates(nums):
+    if not nums:
+        return 0
+
+    i = 0
+
+    for j in range(1, len(nums)):
+        if nums[i] != nums[j]:
+            i += 1
+            nums[i] = nums[j]
+
+    return i + 1
+
+
+# Example usage
+nums = [1, 1, 2, 2, 3, 4, 4]
+length = remove_duplicates(nums)
+
+print("New length:", length)
+print("Updated list:", nums[:length])
